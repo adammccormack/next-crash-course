0dabc101a48b7172565fb672815540f8b2deaa80
amccormack <79875785+adammccormack@users.noreply.github.com>
2022-12-19 11:02:47 +0000

    added Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
       <div>
+        <Head>
+          <title>All The Newz</title>
+          <meta name='keyswords' content='news, news articles' />
+        </Head>
         <h1>Welcome to NEXT</h1>
       </div> 
   )
